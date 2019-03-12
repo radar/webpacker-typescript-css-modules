4f8f94e66ccbf2323059646740e05d038ff1ec45
Ryan Bigg <me@ryanbigg.com>
2019-03-12 21:45:37 +1100

    Typescript cannot find CSS modules

---
+++ app/javascript/packs/hello_react.tsx
@@ -0,0 +1,12 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import * as React from 'react'
+import styles from '../hello_react/styles/hello-react'
+
+const Hello = props => (
+  <div className={styles.helloReact}>Hello {props.name}!</div>
+)
+
+export default Hello
